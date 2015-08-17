a3f39f237f8806841c9e959174ccfd1d346d2655
Ahsan Ellahi <ahsan.ellahi@square63.com>
2015-08-17 13:47:22 +0500

    User : Associated with image model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_one :image, as: :imageable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :confirmable, :validatable
 
