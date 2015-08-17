8ec87aba420b4716969fe5348dfba489e52fded0
Ahsan Ellahi <ahsan.ellahi@square63.com>
2015-08-17 10:35:15 +0500

    Root URL : Changed to user/sign_in & index.html.erb was deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DemoProject::Application.routes.draw do
   devise_for :users
 
+  root to: 'devise/sessions#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
