5d28ba3394376228a1a7a378af25da8618085024
Ahsan Ellahi <ahsan.ellahi@square63.com>
2015-08-24 16:49:40 +0500

    Order: Removed address attribute

--- db/migrate/20150824100222_add_column_to_orders.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToOrders < ActiveRecord::Migration
-  def up
-    add_column :orders, :address, :text, null: false
-  end
-
-  def down
-    remove_column :orders, :address
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150824100222) do
+ActiveRecord::Schema.define(:version => 20150824080224) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(:version => 20150824100222) do
     t.float    "total"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.text     "address",    :null => false
   end
 
   create_table "products", :force => true do |t|
