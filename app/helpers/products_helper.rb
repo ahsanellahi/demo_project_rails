7c36d3a82a54ad66831360e1405e75540eb0ae82
Ahsan Ellahi <ahsan.ellahi@square63.com>
2015-08-24 10:02:34 +0500

    Views: Added styling user dashboard, user show and product show page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -7,6 +7,10 @@ module ProductsHelper
     user_signed_in? && is_owner?(user)
   end
 
+  def is_owner_or_not_signed_in? user
+    !(user_signed_in?) || is_owner?(user)
+  end
+
   def get_product_image_url image, size
     image ? image.get_image_url(size) : 'default-image.png'
   end
