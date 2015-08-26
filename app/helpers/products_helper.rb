0ff918a70d34e118213dc0f9c4ddc954ec8d05e3
Ahsan Ellahi <ahsan.ellahi@square63.com>
2015-08-26 13:21:11 +0500

    Product: Front-end restriction to add already added product in cart

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -14,4 +14,8 @@ module ProductsHelper
   def get_product_image_url image, size
     image ? image.get_image_url(size) : 'default-image.png'
   end
+
+  def product_in_cart? product
+    current_order.products.include?(product)
+  end
 end
