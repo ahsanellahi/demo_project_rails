dada6c6288088cfb7f637a19831a518919a120cd
Ahsan Ellahi <ahsan.ellahi@square63.com>
2015-08-28 10:58:15 +0500

    Cart: Displayed checkout button for unsigned user

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,4 +2,8 @@ module CartsHelper
   def user_signed_in_and_has_order?
   user_signed_in? && current_user.orders.where(purchased_at: nil).last.products.first
   end
+
+  def has_order?
+    current_order.products.first
+  end
 end
